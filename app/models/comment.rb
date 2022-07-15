163560915aaa4aa07cf0a82900f24ad46ce5b426
Adekunle Ismail <adismail4@gmail.com>
2022-07-15 13:10:06 +0100

    Add customs method to classes and rename table column

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  def update_comment_counter
+    post.increament(:comment_counter)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :like
+  has_many :comment
+
+  def update_counter
+    user.increment(:post_counter)    
+  end
+
+  def recent_comment
+    comment.order(:created_on, :desc).limit(5)
+  end
 end
