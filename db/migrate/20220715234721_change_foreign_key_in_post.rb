5b13543ed2c10004589d033457c7be1d4f2a3b78
Adekunle Ismail <adismail4@gmail.com>
2022-07-16 09:20:34 +0100

    Change user_id to author_id

--- db/migrate/20220715234721_change_foreign_key_in_post.rb
+++ db/migrate/20220715234721_change_foreign_key_in_post.rb
@@ -1,4 +1,4 @@
-class RenameUseridInPosts < ActiveRecord::Migration[7.0]
+class ChangeForeignKeyInPost < ActiveRecord::Migration[7.0]
   def change
     rename_column :posts, :user_id, :author_id
   end
