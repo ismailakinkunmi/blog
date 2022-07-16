cef2b2d4f4b6d61210ea050b850f66053137c288
Adekunle Ismail <adismail4@gmail.com>
2022-07-16 23:23:27 +0100

    Add rspec and re-test

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '3.1.2'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'rails', '~> 7.0.3'
+gem 'ffi'
 
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem 'sprockets-rails'
@@ -51,6 +52,7 @@ gem 'bootsnap', require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
+  gem 'rspec-rails'
 end
 
 group :development do
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validaiton tests for user' do
+    subject { User.new(name: 'ismail', photo: 'profile.png', bio: 'ismail the great', post_counter: 0) }
+    before { subject.save }
+
+    it 'Shuold have name present' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'Should have post counter as an integer' do
+      subject.post_counter = 3
+      expect(subject).to be_valid
+    end
+
+    it 'Should have post counter not as float' do
+      subject.post_counter = 3.5
+      expect(subject).to_not be_valid
+    end
+
+    it 'Should have post counter not be less than zero' do
+      subject.post_counter = -1
+      expect(subject).to_not be_valid
+    end
+
+    it 'Should have the subject as valid' do
+      expect(subject).to be_valid
+    end
+  end
+
+  describe 'Test for recent comments on posts' do
+    subject do
+      ismail = User.new(name: 'ismail', photo: 'profile.png',
+                       bio: 'I am coming out to be the best computer programmer', posts_counter: 0)
+      Post.new(title: 'My best friend', text: 'He will be unvailed at the end of my program at Microverse',
+               comment_counter: 3, like_counter: 3, author_id: ismail.id)
+    end
+   
+  end
+end
