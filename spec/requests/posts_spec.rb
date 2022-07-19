e04e968cb03f619cc6ef7893c6284b71c7ec3bd9
Adekunle Ismail <adismail4@gmail.com>
2022-07-19 18:39:25 +0100

    Run rubocop -A to fix linter errors

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,21 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe "PostsController", type: :request do
-  describe "GET posts" do
+RSpec.describe 'PostsController', type: :request do
+  describe 'GET posts' do
     it "should render 'index.html.erb' view" do
       get '/users/1/posts'
       expect(response.body).to include('list of posts for a given user')
     end
 
-    it "should render users/1/posts/index" do
+    it 'should render users/1/posts/index' do
       get '/users/1/posts'
       expect(response).to have_http_status(200)
       expect(response).to render_template('index')
     end
   end
 
-  describe "Get post/1" do
-
+  describe 'Get post/1' do
     it "should render 'show.html.erb' view" do
       get '/users/1/posts/1'
       expect(response.body).to include('Get post by id')
@@ -25,6 +24,5 @@ RSpec.describe "PostsController", type: :request do
       get '/users/1/posts/1'
       expect(response.body).to render_template('show')
     end
-    
   end
 end
