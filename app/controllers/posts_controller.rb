4b017264a422389cd912ddd235dbffd530607f38
Adekunle Ismail <adismail4@gmail.com>
2022-07-21 22:08:04 +0100

    Add index and show action to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,12 @@
 class PostsController < ApplicationController
+  
   def index
-    @posts = Post.all
+    @user = User.find(params[:user_id])
+    @posts = @user.posts    
   end
 
   def show
-    @post = Post.find(params[:id])
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:id]) 
   end
 end
