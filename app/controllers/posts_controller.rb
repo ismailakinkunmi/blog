8677b99838693a8ecc10f02e270e0515f7823d0c
Adekunle Ismail <adismail4@gmail.com>
2022-07-21 22:27:45 +0100

    Run rubocop -A to fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,11 @@
 class PostsController < ApplicationController
-  
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts    
+    @posts = @user.posts
   end
 
   def show
     @user = User.find(params[:user_id])
-    @post = Post.find(params[:id]) 
+    @post = Post.find(params[:id])
   end
 end
