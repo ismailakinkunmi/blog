cef17bc8d6f8f629bd86fa91187e6ff355317155
Adekunle Ismail <adismail4@gmail.com>
2022-07-28 00:08:51 +0100

    Add devise route and configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts
+  devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+      #resources :posts
   root "users#index"
 
   resources :users, only: %i[index show] do
