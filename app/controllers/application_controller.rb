2bd06f2c7f283c5c4ad1c277d912e3cc95e5db6e
Adekunle Ismail <adismail4@gmail.com>
2022-07-28 01:42:02 +0100

    Run rubocop -A to fix errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,12 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-
-   protected
+  protected
 
   def configure_permitted_parameters
     attributes = %i[name email password]
     devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
     devise_parameter_sanitizer.permit(:account_update, keys: attributes)
   end
-
 end
